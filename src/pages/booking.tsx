02cdc5a9c4546ddd0609ae99be16c210c19a7a76
Frédéric Barthelet <frederic.barthelet@gmail.com>
2025-03-03 17:47:42 +0100

    Add time column

--- src/pages/booking.tsx
+++ src/pages/booking.tsx
@@ -11,15 +11,23 @@ const BookingPage = () => {
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Book a zoom call</h1>
 
-      <div className="grid grid-cols-7 gap-4">
+      <div className="grid grid-cols-8">
+        <div className="text-center  border-r">
+          <div className="h-16"></div>
+          {Array.from({ length: 12 }, (_, i) => (
+            <div key={`time-${i}`} className="p-4">
+              <div className="text-center">
+                {dayjs().hour(9).minute(0).add(i, "hour").format("hh:mm A")}
+              </div>
+            </div>
+          ))}
+        </div>
         {Array.from({ length: 7 }, (_, i) =>
           dayjs().startOf("isoWeek").add(i, "day")
         ).map((day) => (
-          <div key={day.toString()} className="border rounded-lg p-4">
-            <div className="text-center">
-              {/* Display day name */}
+          <div key={day.toString()} className="p-4 text-center border-r">
+            <div className="h-16">
               <div className="font-semibold">{day.format("dddd")}</div>
-              {/* Display date */}
               <div className="mt-1">{day.format("MMM D")}</div>
             </div>
           </div>
